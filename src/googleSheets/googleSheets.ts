614b49ad6d1fee440e9ce98f0cbd89e606eb96b1
kim-haram-lvgs <haram.kim@leverages.jp>
2022-05-31 08:56:10 +0900

    feat(GoogleSheets)!: Converted googleSheets to TS
    feat: converted googleSheets.js to typescript
    - file extension changed to .ts
    - converted CommonJS syntax to ES Modules syntax
    - added type annotations
    
    BREAKING CHANGE: File extension of googleSheets.js to .ts

--- src/googleSheets/googleSheets.ts
+++ src/googleSheets/googleSheets.ts
@@ -1,14 +1,23 @@
+//import relevant type definitions from types/types.ts
+// import type {
+//     GoogleApiConfig,
+// } from '../types/types';
+
 //import google API related packages
-const { google } = require ('googleapis');
+import {
+    google,
+    sheets_v4, //for type declaration
+    // GaxiosResponse,
+} from 'googleapis';
 // const { resolve } = require('path');
 //const { sheets } = require('googleapis/build/src/apis/sheets');
 
 const googleAPI = {
     //auth to Google API and return obj with scope for Google Sheets API
-    authForSheets: async (googleConfig) => {
-        let sheets;
+    authForSheets: async (googleConfig: GoogleApiConfig) => {
+        let sheets:sheets_v4.Sheets;
         try{
-            // const authClient = await google.auth.getClient(
+            // using service account with 'keyFile' property = https://www.npmjs.com/package/googleapis#service-account-credentials
             const authClient = await google.auth.getClient(googleConfig);
             sheets = google.sheets({ version: 'v4', auth: authClient });
             console.log('Authenticated to Google Sheets API using service account.');
@@ -21,8 +30,8 @@ const googleAPI = {
 
 
 const googleSheets = {
-    readTargetSheet: async (sheets, sheetsReadRequest) => {
-        let result;
+    readTargetSheet: async (sheets: sheets_v4.Sheets, sheetsReadRequest: sheets_v4.Params$Resource$Spreadsheets$Values$Get) => {
+        let result; //LATER - need to figure out type notation from dependency npm: GaxiosResponse<sheets_v4.Schema$ValueRange>
         try{
             result = await sheets.spreadsheets.values.get(sheetsReadRequest);
         }catch(err){
@@ -32,4 +41,4 @@ const googleSheets = {
     },
 }
 
-module.exports = {googleAPI, googleSheets};
+export {googleAPI, googleSheets};
