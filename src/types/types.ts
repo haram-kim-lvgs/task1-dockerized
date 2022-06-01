26528473609426187cf403b3e16a408ded63a2a5
kim-haram-lvgs <haram.kim@leverages.jp>
2022-06-01 14:21:04 +0900

    feat(types): Added interface definitions
    Decided to prefer interfaces over type alias when possible based on below 2 articles
    - https://ncjamieson.com/prefer-interfaces/
    - https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,14 +1,19 @@
 //holds type definition
+    //prefer interface when possible, over type 
+    // https://ncjamieson.com/prefer-interfaces/
+    // https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections
 
 //config --> explicitly defining to not having to access property through bracket, but through dot notation
     // https://stackoverflow.com/questions/38324949/error-ts2339-property-x-does-not-exist-on-type-y?rq=1
-/*export*/ type Config = {
+// type Config = {
+interface Config {
     db: MysqlConfig,
     googleApi: GoogleApiConfig,
 }
 
 //mysql db config
-/*export*/ type MysqlConfig = {
+// type MysqlConfig = {
+interface MysqlConfig {
     host: string,
     port: string,
     database: string,
@@ -18,9 +23,19 @@
 }
 
 //google api config
-/*export*/ type GoogleApiConfig = {
+// type GoogleApiConfig = {
+interface GoogleApiConfig {
     keyFile: string,
     scopes: string[]
 }
 
-// export type {Config};
+//google sheets api for reading range
+// type SheetsReadRequest = {
+interface SheetsReadRequest {
+    spreadsheetId: string,
+    range: string
+}
+
+//use as module to prevent 'global pollution' e.g., avoiding duplicate identifier
+//also use named export/import && avoid using 'as' keyword when importing for maintainability & future-proofing
+export type {Config, MysqlConfig, GoogleApiConfig, SheetsReadRequest}; 
