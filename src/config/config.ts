88ad8827350e87d5309fd5aec6a0ae8ee8ac7ad0
kim-haram-lvgs <haram.kim@leverages.jp>
2022-06-06 16:01:26 +0900

    feat(config): Adjust to avoid type coercion
    - Adjusted types to avoid type coercion by javascript, following guide of Typescript
    - Adjusted comments

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,6 @@
 //import relevant type definitions from types/types.ts
 import type {
     Config,
-//     MysqlConfig,
-//     GoogleApiConfig,
 } from '../types/types';
 
 import path from 'path';
@@ -13,21 +11,20 @@ dotenv.config({path: `${__dirname}/../../.env`});
 
 //config object
 const env = process.env;
-// const config = <Config> { //using type assertion
+//const config = <Config> { //using type assertion
 const config: Config = { //using type notation
-    //db config
-    // db : <MysqlConfig> { //this might be using type assertion.....
     db : { //this might be using type inference && field name should be identical to how they are defined in type
         //for host, use db name defined in docker-compose.yml
         host    : env.DB_HOST ?? 'localhost',
-        port    : env.DB_PORT ?? '3306',
+        port    : Number(env.DB_PORT) ?? 3306,
         database: env.DB_NAME ?? 'default_database',
         user    : env.DB_USER ?? 'root',
         password: env.DB_PASSWORD ?? 'password',
-        // protocol: 'tcp',
+        //connectionLimit: Number(env.DB_CONNECTION_LIMIT) ?? 10,
+        //protocol: 'tcp',
         },
     //google API config
-    // googleApi : <GoogleApiConfig> { // this might be using type assertion....
+    //googleApi : <GoogleApiConfig> { //using type assertion....
     googleApi : { //this might be using type inference && field name should be identical to how they are defined in type
         keyFile: path.resolve(`${__dirname}/../../${process.env.GOOGLE_SECRET_PATH}`),
         scopes: [process.env.GOOGLE_API_SCOPE as string]
@@ -35,5 +32,5 @@ const config: Config = { //using type notation
 }
 
 //export mysql db config
-export {config}; //exporting this way, in ES Module syntax, the object is being exported with its values and related types intact
-// export default {config}; //exporting this way, in ES Module, the export can be imported with default import, which only carries value, and not related types (and TS wont let you use dot notation to access properties, but only bracket notation)
+export {config}; //exporting this way, in ES Module syntax, the object is being exported with its values and related types intact [??]
+// export default {config}; //exporting this way, in ES Module, the export can be imported with default import, which only carries value, and not related types (and in this case, TS wont let you use dot notation to access properties, but only bracket notation) [??]
