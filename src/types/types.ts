c9dff8408ed4917e10f192b0c994306208bf2bcc
kim-haram-lvgs <haram.kim@leverages.jp>
2022-06-07 08:57:20 +0900

    chore(types): Added and cleaned up comments

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,11 @@
 //holds type definition
     //prefer interface when possible, over type 
-    // https://ncjamieson.com/prefer-interfaces/
-    // https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections
+        //https://google.github.io/styleguide/tsguide.html#interfaces-vs-type-aliases
+        //https://ncjamieson.com/prefer-interfaces/
+        //https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections
 
-//config --> explicitly defining to not having to access property through bracket, but through dot notation
-    // https://stackoverflow.com/questions/38324949/error-ts2339-property-x-does-not-exist-on-type-y?rq=1
+//config --> explicitly defining to not having to access property through bracket notation, but through dot notation
+    //https://stackoverflow.com/questions/38324949/error-ts2339-property-x-does-not-exist-on-type-y?rq=1
 // type Config = {
 interface Config {
     db: MysqlConfig,
@@ -13,13 +14,22 @@ interface Config {
 
 //mysql db config
 // type MysqlConfig = {
+    //subset of mysql.PoolOptions = https://github.com/mysqljs/mysql#pool-options
+    //which is superset of ConnectionOptions = https://github.com/mysqljs/mysql#connection-options
 interface MysqlConfig {
     host: string,
-    port: string,
+    port: number,
     database: string,
     user: string,
     password: string,
-    protocol?:string,
+    //connectionLimit: number, //number of connections to create at once 
+    // waitForConnections: boolean,   //default = true
+    //                                 //with both waitForConnection as true & queueLimit, the current error might NOT occur, if given sufficient timeout duration..?
+    //                                 //BUT should rather implement .connect() retry wih setTimeout(), because too long of timeout is not good in prod env
+    // queueLimit: number,
+    // connectTimeout: , //default 10000 = 10sec 
+    // acquireTimeout: , //default 10000 = 10sec
+    // protocol?:string,
 }
 
 //google api config
@@ -36,6 +46,8 @@ interface SheetsReadRequest {
     range: string
 }
 
-//use as module to prevent 'global pollution' e.g., avoiding duplicate identifier
+//use as module (have top level import or export in the file, even if 'export {};') to prevent 'global pollution' e.g., avoiding duplicate identifier
 //also use named export/import && avoid using 'as' keyword when importing for maintainability & future-proofing
+    //https://stackoverflow.com/questions/46913851/why-and-when-to-use-default-export-over-named-exports-in-es6-modules
+    //https://engineering.linecorp.com/ja/blog/you-dont-need-default-export/
 export type {Config, MysqlConfig, GoogleApiConfig, SheetsReadRequest}; 
