2332db9d47cda08e874083546ae870e4f33ebe09
kim-haram-lvgs <haram.kim@leverages.jp>
2022-06-07 08:54:27 +0900

    chore(mysql): Added comments around mysql
    Added comments around mysql functions and reasons behind the choices

--- src/mysql/mysql.ts
+++ src/mysql/mysql.ts
@@ -1,6 +1,46 @@
 
 import mysql from 'mysql2/promise';
 
+//generic reference for error handling of npm mysql
+    //https://github.com/mysqljs/mysql#error-handling
+
+//use .execute instead of .query to take advantage of automatic prepared statement (will cache the statements)
+    //https://github.com/sidorares/node-mysql2/blob/master/documentation/Prepared-Statements.md
+        //to close cache statement = connection.unprepare(<sql_query_with_?>); 
+            //for manually controlling statements and queries
+            //https://github.com/sidorares/node-mysql2/blob/master/documentation/Prepared-Statements.md#manual-prepare--execute
+
+// mysql2 comes with prepared statements, which can protect from SQL injection attacks
+    //http://stackoverflow.com/questions/8263371/how-can-prepared-statements-protect-from-sql-injection-attacks
+    // https://github.com/sidorares/node-mysql2#using-prepared-statements
+        // for more detail & syntax for manual prepare syntax 
+        //https://github.com/sidorares/node-mysql2/blob/master/documentation/Prepared-Statements.md     
+
+//with npm mysql2, comes with connection.execute helper
+    // connection.query vs connection.execute 
+                //https://stackoverflow.com/questions/53197922/difference-between-query-and-execute-in-mysql
+                    // .query() = parameter substitution is handled on the client, can handle objects
+                    // = .execute() = helper that combines statement prepare + query --> client prepare statement as serialized string and server handles the rest
+
+//mysql explicit connection vs implicit connection
+    // documentation = https://github.com/mysqljs/mysql#establishing-connections
+
+//manual connection could give more granular control over connections,
+    //BUT when combined with the need to also manually prepare (and optionally unprepare) statements for added protection against sql injection attacks,
+    //taking advantage of helper function .execute() which comes with implicit connection and prepared statement deemed to be more valuable
+        //the impact of lose of granular control over connections could be alleviated delegated to mysql itself by using connection pool instead of single connection
+    //also the potential issue of order-sensitive queries being executed in parallel with multiple connections can be controlled through proper async/await & promise
+    //BUT if it is required that a series of queries do NOT share connections or switch connection between queries,
+        //it might be required to manually handle the connections again, as the .query() with implicit connection will automatically release the connection after each query
+
+        //https://github.com/mysqljs/mysql#pooling-connections
+        // --> mysql.createPool() -> pool.query() 
+        // is shorthand for 
+        // --> mysql.createPool() -> pool.getConnection() -> connection.query() -> connection.release() 
+            //if want to close connection AND remove it, instead of just releasing = use connection.destroy() instead of connection.release()
+        // closing all the connections in a pool = use .end()
+            //https://github.com/mysqljs/mysql#closing-all-the-connections-in-a-pool
+
 
 //init MYSQL table
 export async function initTable (dbConnection: mysql.Pool, dbTableName: string, retry: number = 0, interval: number = 5000):Promise<any> {
