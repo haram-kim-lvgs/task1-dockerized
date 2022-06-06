80e67f293d91b48eaa7a6f085a56536e3d941611
kim-haram-lvgs <haram.kim@leverages.jp>
2022-06-07 08:56:25 +0900

    chore(googlesheets): Cleaned up comments

--- src/googleSheets/googleSheets.ts
+++ src/googleSheets/googleSheets.ts
@@ -3,20 +3,17 @@ import type {
     GoogleApiConfig,
 } from '../types/types';
 
-//import type-only.... from a sub dependent package for type handling....(??)
+//import type-only.... from a sub dependent package for type handling....
+    //unsure how this portion is working... [Q]
 import type {
     GaxiosResponse
 } from 'gaxios';
 
 //import google API related packages
-// import gapis, { //syntax to combine importing of default and named at the same time
 import {
     google,
     sheets_v4, //for type declaration
-    // GaxiosResponse,
 } from 'googleapis';
-// const { resolve } = require('path');
-//const { sheets } = require('googleapis/build/src/apis/sheets');
 
 const googleAPI = {
     //auth to Google API and return obj with scope for Google Sheets API
@@ -34,10 +31,9 @@ const googleAPI = {
     },
 }
 
-
 const googleSheets = {
     readTargetSheet: async (sheets: sheets_v4.Sheets, sheetsReadRequest: sheets_v4.Params$Resource$Spreadsheets$Values$Get) => {
-        // let result: sheets_v4.Sheets; //resolved the type notation through type-only import from sub-dependency npm: GaxiosResponse<sheets_v4.Schema$ValueRange>
+        //resolved the type notation through type-only import from sub-dependency npm: GaxiosResponse<sheets_v4.Schema$ValueRange>
         let result: GaxiosResponse<sheets_v4.Schema$ValueRange>; 
         try{
             result = await sheets.spreadsheets.values.get(sheetsReadRequest);
